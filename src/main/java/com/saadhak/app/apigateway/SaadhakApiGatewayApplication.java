0e2515ee357ed344377c2c24514685c55944fd8b
nedate1976 <ndate1976@hotmail.com>
2024-01-12 21:53:10 +0530

    initial commit

---
+++ src/main/java/com/saadhak/app/apigateway/SaadhakApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.saadhak.app.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaadhakApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaadhakApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadhak/app/apigateway/SaadhakApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadhak.app.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaadhakApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
